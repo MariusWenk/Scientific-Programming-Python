b60f6087f32675877c773e669dec643c30d51a97
MariusWenk <Marius_Wenk@gmx.de>
2022-02-24 13:09:35 +0100

    1

--- FP/1.-Zeeman.py
+++ FP/1.-Zeeman.py
@@ -34,19 +34,33 @@ yerr = []
 for i in range(countFiles):
     s.append(beamData[i][:,0].size)
     x_data.append(beamData[i][:,5])
-    xerr.append([1 for j in range(s[i])])
     y_data.append(beamData[i][:,6])
-    yerr.append([1 for j in range(s[i])])
     
 r = []
+square_r_err = []
 for i in range(countFiles):
     x0 = x_data[i][0]
     y0 = y_data[i][0]
     le = x_data[i].size
+    if i<12:
+        messung = 12 + i//3
+    else:
+        messung = 8 + i//3
+    if messung < 14:
+        delx0 = 3
+        dely0 = 3
+        delrk = 3.7
+    else:
+        delx0 = 2
+        dely0 = 2
+        delrk = 2.16
     pre_r = []
+    pre_r_square_err = []
     for j in range(1,le):
         pre_r.append(np.sqrt(((x_data[i][j]-x0)**2)+((y_data[i][j]-y0)**2)))
+        pre_r_square_err.append((2*abs(x_data[i][j]-x0)*delx0)+(2*abs(y_data[i][j]-y0)*dely0)+delrk)
     r.append(np.array(pre_r))
+    square_r_err.append(np.array(pre_r_square_err))
     
 square_r = np.array(r)**2
 
@@ -61,6 +75,17 @@ for i in range(countFiles//2):
     square_cut.append(np.array([square_r[i][::2][:ma],square_r[i+countFiles//2][:ma],square_r[i][1::2][:ma]]))
     delta.append(square_cut[i][1:,:] - square_cut[i][:-1,:])
     
+square_err = []
+square_cut_err = []
+Delta_err = []
+delta_err = []
+for i in range(countFiles//2):
+    ma = min(square_r_err[i][::2].size,square_r_err[i+countFiles//2].size,square_r_err[i][1::2].size)
+    square_err.append([square_r_err[i][::2],square_r_err[i+countFiles//2],square_r_err[i][1::2]])
+    Delta_err.append([a[1:] + a[:-1] for a in [square_r_err[i][::2],square_r_err[i+countFiles//2],square_r_err[i][1::2]]])
+    square_cut_err.append(np.array([square_r_err[i][::2][:ma],square_r_err[i+countFiles//2][:ma],square_r_err[i][1::2][:ma]]))
+    delta_err.append(square_cut_err[i][1:,:] + square_cut_err[i][:-1,:])
+    
 """ Daten in Tabelle ausgeben """
 fig = []
 ax = []
@@ -74,13 +99,31 @@ for i in range(countFiles//2):
     b.append(np.around(list(mi.roundrobin(square[i][1],Delta[i][1])),3).tolist()+[""]*((ma*2)-1-len(list(mi.roundrobin(square[i][1],Delta[i][1])))))
     b.append(list(mi.roundrobin(np.around(delta[i][1],3),zeros[1]))+[""]*((ma*2)-1-len(list(mi.roundrobin(delta[i][1],zeros[1])))))
     b.append(np.around(list(mi.roundrobin(square[i][2],Delta[i][2])),3).tolist()+[""]*((ma*2)-1-len(list(mi.roundrobin(square[i][2],Delta[i][2])))))
-    labels = ["","","","","",""]
+    labels = ["Ordnung","innerste","","mittlere","","äußerste"]
     b = np.array(b).T
     fig.append(plt.figure())
     ax.append(fig[i].add_axes([0,0,1,1]))
     ax[i].table(cellText=b,colLabels=labels,loc='center',rowLoc='center')
     ax[i].axis("off")
     ax[i]
+    
+for i in range(countFiles//2):
+    b = []
+    zeros = [["" for i in range(delta[i][0].size)],["" for i in range(delta[i][1].size)]]
+    ma = max(square_r_err[i][::2].size,square_r_err[i+countFiles//2].size,square_r_err[i][::2].size)
+    b.append(["" if i%2==1 else 1+(i//2) for i in range((ma*2)-1)])
+    b.append(np.around(list(mi.roundrobin(square_err[i][0],Delta_err[i][0])),3).tolist()+[""]*((ma*2)-1-len(list(mi.roundrobin(square_err[i][0],Delta_err[i][0])))))
+    b.append(list(mi.roundrobin(np.around(delta_err[i][0],3),zeros[0]))+[""]*((ma*2)-1-len(list(mi.roundrobin(delta_err[i][0],zeros[0])))))
+    b.append(np.around(list(mi.roundrobin(square_err[i][1],Delta_err[i][1])),3).tolist()+[""]*((ma*2)-1-len(list(mi.roundrobin(square_err[i][1],Delta_err[i][1])))))
+    b.append(list(mi.roundrobin(np.around(delta_err[i][1],3),zeros[1]))+[""]*((ma*2)-1-len(list(mi.roundrobin(delta_err[i][1],zeros[1])))))
+    b.append(np.around(list(mi.roundrobin(square_err[i][2],Delta_err[i][2])),3).tolist()+[""]*((ma*2)-1-len(list(mi.roundrobin(square_err[i][2],Delta_err[i][2])))))
+    labels = ["Ordnung","innerste","","mittlere","","äußerste"]
+    b = np.array(b).T
+    fig.append(plt.figure())
+    ax.append(fig[i+countFiles//2].add_axes([0,0,1,1]))
+    ax[i+countFiles//2].table(cellText=b,colLabels=labels,loc='center',rowLoc='center')
+    ax[i+countFiles//2].axis("off")
+    ax[i+countFiles//2]
 
 """ Plot speichern """
 for i in range(countFiles//2):
@@ -90,3 +133,11 @@ for i in range(countFiles//2):
         messung = 8 + i//3
     farbe = farbArray[i%3]
     fig[i].savefig(f"./1.Plots/{messung}{farbe}_table.png", dpi=100) # Bild als png Datei in Ordner Plots gespeichert
+    
+for i in range(countFiles//2):
+    if i<12:
+        messung = 12 + i//3
+    else:
+        messung = 8 + i//3
+    farbe = farbArray[i%3]
+    fig[i+countFiles//2].savefig(f"./1.Plots/{messung}{farbe}_error_table.png", dpi=100) # Bild als png Datei in Ordner Plots gespeichert
